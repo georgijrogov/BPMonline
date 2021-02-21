575a4cca00b4c3334693d23d86123e85f835fa5d
georgijrogov <georgijrogov@gmail.com>
2021-02-21 22:45:44 +0300

    added swimming program section

---
+++ CertificationPkg/Schemas/IteSwimmingProgram21b8e62aSection/IteSwimmingProgram21b8e62aSection.js
@@ -0,0 +1,8 @@
+define("IteSwimmingProgram21b8e62aSection", [], function() {
+	return {
+		entitySchemaName: "IteSwimmingProgram",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
