d1690448afc6c7a91369316e0ba24f84abff6450
georgijrogov <georgijrogov@gmail.com>
2021-02-26 20:06:07 +0300

    Revert "Added field filtration"
    This reverts commit aa805fd9527c9b53a669fbbe4f975563993821e6.

--- CertificationPkg/Schemas/IteSwimmingProgram1Page/IteSwimmingProgram1Page.js
+++ CertificationPkg/Schemas/IteSwimmingProgram1Page/IteSwimmingProgram1Page.js
@@ -13,25 +13,8 @@ define("IteSwimmingProgram1Page", [], function() {
 				}
 			}
 		}/**SCHEMA_DETAILS*/,
-		businessRules: /**SCHEMA_BUSINESS_RULES*/{
-			"IteResponsible": {
-				"e9ac55bc-99f8-4d2b-869a-fc99cb662f0f": {
-					"uId": "e9ac55bc-99f8-4d2b-869a-fc99cb662f0f",
-					"enabled": true,
-					"removed": false,
-					"ruleType": 1,
-					"baseAttributePatch": "Type",
-					"comparisonType": 3,
-					"autoClean": false,
-					"autocomplete": false,
-					"type": 0,
-					"value": "60733efc-f36b-1410-a883-16d83cab0980",
-					"dataValueType": 10
-				}
-			}
-		}/**SCHEMA_BUSINESS_RULES*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
 		methods: {},
-		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
 		diff: /**SCHEMA_DIFF*/[
 			{
 				"operation": "insert",
@@ -52,140 +35,48 @@ define("IteSwimmingProgram1Page", [], function() {
 			},
 			{
 				"operation": "insert",
-				"name": "STRING6419650b-50d8-40ca-983c-aba04a07f00a",
-				"values": {
-					"layout": {
-						"colSpan": 24,
-						"rowSpan": 1,
-						"column": 0,
-						"row": 1,
-						"layoutName": "ProfileContainer"
-					},
-					"bindTo": "IteCode",
-					"enabled": true
-				},
-				"parentName": "ProfileContainer",
-				"propertyName": "items",
-				"index": 1
-			},
-			{
-				"operation": "insert",
-				"name": "LOOKUPc29727ce-a68a-445f-80c3-d7cdf21de325",
-				"values": {
-					"layout": {
-						"colSpan": 24,
-						"rowSpan": 1,
-						"column": 0,
-						"row": 2,
-						"layoutName": "ProfileContainer"
-					},
-					"bindTo": "ItePeriodicity",
-					"enabled": true,
-					"contentType": 5
-				},
-				"parentName": "ProfileContainer",
-				"propertyName": "items",
-				"index": 2
-			},
-			{
-				"operation": "insert",
-				"name": "LOOKUP563a7dad-a999-44c2-aae8-d0895057783e",
-				"values": {
-					"layout": {
-						"colSpan": 24,
-						"rowSpan": 1,
-						"column": 0,
-						"row": 3,
-						"layoutName": "ProfileContainer"
-					},
-					"bindTo": "IteResponsible",
-					"enabled": true,
-					"contentType": 5
-				},
-				"parentName": "ProfileContainer",
-				"propertyName": "items",
-				"index": 3
-			},
-			{
-				"operation": "insert",
-				"name": "BOOLEANd5ec476c-3c0b-4874-8142-0c796fe8d43d",
-				"values": {
-					"layout": {
-						"colSpan": 24,
-						"rowSpan": 1,
-						"column": 0,
-						"row": 4,
-						"layoutName": "ProfileContainer"
-					},
-					"bindTo": "IteIsActive",
-					"enabled": true
-				},
-				"parentName": "ProfileContainer",
-				"propertyName": "items",
-				"index": 4
-			},
-			{
-				"operation": "insert",
-				"name": "IteCommentad19def2-b75d-4a34-be0f-e72233199940",
-				"values": {
-					"layout": {
-						"colSpan": 24,
-						"rowSpan": 1,
-						"column": 0,
-						"row": 0,
-						"layoutName": "Header"
-					},
-					"bindTo": "IteComment"
-				},
-				"parentName": "Header",
-				"propertyName": "items",
-				"index": 0
-			},
-			{
-				"operation": "insert",
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0,
 				"name": "NotesAndFilesTab",
 				"values": {
 					"caption": {
 						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
 					},
-					"items": [],
-					"order": 0
-				},
-				"parentName": "Tabs",
-				"propertyName": "tabs",
-				"index": 0
+					"items": []
+				}
 			},
 			{
 				"operation": "insert",
-				"name": "Files",
-				"values": {
-					"itemType": 2
-				},
 				"parentName": "NotesAndFilesTab",
 				"propertyName": "items",
-				"index": 0
+				"name": "Files",
+				"values": {
+					"itemType": Terrasoft.ViewItemType.DETAIL
+				}
 			},
 			{
 				"operation": "insert",
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
 				"name": "NotesControlGroup",
 				"values": {
-					"itemType": 15,
+					"itemType": Terrasoft.ViewItemType.CONTROL_GROUP,
 					"caption": {
 						"bindTo": "Resources.Strings.NotesGroupCaption"
 					},
 					"items": []
-				},
-				"parentName": "NotesAndFilesTab",
-				"propertyName": "items",
-				"index": 1
+				}
 			},
 			{
 				"operation": "insert",
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
 				"name": "Notes",
 				"values": {
 					"bindTo": "IteNotes",
-					"dataValueType": 1,
-					"contentType": 4,
+					"dataValueType": Terrasoft.DataValueType.TEXT,
+					"contentType": Terrasoft.ContentType.RICH_TEXT,
 					"layout": {
 						"column": 0,
 						"row": 0,
@@ -202,16 +93,6 @@ define("IteSwimmingProgram1Page", [], function() {
 							"bindTo": "NotesImagesCollection"
 						}
 					}
-				},
-				"parentName": "NotesControlGroup",
-				"propertyName": "items",
-				"index": 0
-			},
-			{
-				"operation": "merge",
-				"name": "ESNTab",
-				"values": {
-					"order": 1
 				}
 			}
 		]/**SCHEMA_DIFF*/
